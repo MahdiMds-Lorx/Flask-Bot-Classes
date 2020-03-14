a10ff2832e4d436c2d22fb475dff8063ee19796d
mostafa-py <61285975+mostafa-py@users.noreply.github.com>
2020-03-14 17:17:33 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-
+from requests import post
 app = Flask(__name__)
 
 class Message:
@@ -25,23 +25,17 @@ class Bot:
 		self.url = f"https://api.telegram.org/bot{token}/"
 
 	def call(method, **kwargs):
-		ar = ""
-		n = len(kwargs.keys())
-		i = 0
-		for en,key,val in zip(kwargs.keys(), kwargs.values()):
-			i+=1
-			if i == n:
-				ar +=f"{key}={val}"
-			else:
-				ar +=f"{key}={val}&"
-		u = f"{self.url}{method}?{ar}"
-		return u 
+		u = f"{self.url}{method}
+		res = post(u, data=kwargs)
+		data = eval(res.text())
+		return str(data)
 
 	def send_message(self, chat_id, text, parse_mode='markdown', disable_web_page_preview=True, disable_notification=False, reply_to_message_id=None, reply_markup=None,schedule_date=None):
 		try:
 			u = self.call('sendMessage',chat_id=chat_id, text=text, parse_mode=parse_mode, disable_web_page_preview=disable_web_page_preview, disable_notification=disable_notification, reply_to_message_id=reply_to_message_id, schedule_date=schedule_date ,reply_markup=reply_markup)
-		except:
-			pass
+			
+		except Exception as ex:
+			return str(ex)
 		
 	def forward_message(self, chat_id, from_chat_id, message_ids, disable_notification=False , as_copy=False):
 		try:
@@ -50,91 +44,91 @@ class Bot:
 					u = self.call('forwardMessage',chat_id=chat_id, from_chat_id=from_chat_id, message_id=msg_id, disable_notification=disable_notification , as_copy=as_copy)
 			else:
 				u = self.call('forwardMessage',chat_id=chat_id, from_chat_id=from_chat_id, message_id=msg_id, disable_notification=disable_notification,as_copy=as_copy)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 		
 	def send_photo(self , chat_id , photo , file_ref=None , caption=None , parse_mode = "markdown" , disable_notification=False , reply_to_message_id=None , schedule_time=None , reply_markup=None):
 		try:
 			u = self.call('sendPhoto' , chat_id=chat_id , photo = photo , file_ref=file_ref , caption=caption , parse_mode=parse_mode , disable_notification=disable_notification , reply_to_message_id=reply_to_message_id , reply_markup=reply_markup)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 		
 	def send_document(self , chat_id , document , file_ref=None , thumb=None , caption=None,parse_mode="markdown" , disable_notification=False , reply_to_message_id=None , schedule_date=None,reply_markup=None):
 		try:
 			u = self.call('sendDocument' , chat_id=chat_id , document=document , file_ref=file_ref , thumb=thumb , caption=caption , parse_mode=parse_mode , disable_notification=disable_notification , reply_to_message_id=reply_to_message_id , reply_markup=reply_markup , schedule_date=schedule_date)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 		
 	def set_chat_photo(chat_id , photo):
 		try:
 			u = self.call('setChatPhoto' , chat_id=chat_id , photo=photo)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def delete_chat_photo(chat_id):
 		try:
 			u = self.call('deleteChatPhoto' , chat_id=chat_id)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def get_chat(chat_id):
 		try:
 			return self.call('getChat' , chat_id=chat_id)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def get_chat_members_count(chat_id):
 		try:
 			return self.call('getChatMembersCount' , chat_id=chat_id)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def pin_message(chat_id , message_id , disable_notification=False):
 		try:
 			u = self.call('pinChatMessage' , chat_id = chat_id , message_id=message_id , disable_notification=disable_notification)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def unpin_message(chat_id):
 		try:
 			u = self.call('unpinChatMessage' , chat_id=chat_id)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def export_link(chat_id)
 		try:
 			return self.call('exportChatInviteLink' , chat_id=chat_id)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def kick(chat_id , user_id , until_date=None):
 		try:
 			u = self.call('kickChatMember' , chat_id=chat_id , user_id=user_id , until_date=until_date)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def unban(chat_id , user_id):
 		try:
 			u = self.call('unbanChatMember' , chat_id = chat_id ,user_id=user_id)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def leave_chat(chat_id):
 		try:
 			self.call('leaveChat' , chat_id=chat_id)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def edit_message(chat_id , message_id , text , parse_mode="markdown" , disable_web_page_preview=True , reply_markup=None):
 		try:
 			u = self.call('editMessageText' , chat_id=chat_id,message_id=message_id,text=text,parse_mode=parse_mode,disable_web_page_preview=disable_web_page_preview,reply_markup=reply_markup)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def edit_message_caption(chat_id , message_id , caption , parse_mode="markdown"  , reply_markup=None):
 		try:
 			u = self.call('editMessageCaption' , chat_id=chat_id,message_id=message_id,caption=caption,parse_mode=parse_mode,reply_markup=reply_markup)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def edit_message_keyboard(chat_id , message_id , reply_markup=None):
 		try:
 			u = self.call('editMessageReplyMarkup' , chat_id=chat_id,message_id=message_id,reply_markup=reply_markup)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 	def send_action(chat_id , action):
 		try:
 			u = self.call('sendChatAction' , chat_id = chat_id , action=action)
-		except:
-			pass
+		except Exception as ex:
+			return str(ex)
 @app.route("/", methods=['GET', 'POST'])
 def home():
 
